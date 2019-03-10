580c8d25f558a93aaae2cede502985e513088b73
Deniz Ugurlu <mail@denizugurlu.de>
2019-03-10 14:37:52 +0100

    mark gradlew as executable (+x) and add circleci build badge

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![CircleCI](https://circleci.com/gh/dugurlu/spring5-recipe-app/tree/reactive.svg?style=svg)](https://circleci.com/gh/dugurlu/spring5-recipe-app/tree/reactive)
+
 # spring5-reactive-mongo-recipe-app
 Reactive Recipe Application Using MongoDB
 
--- src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -4,11 +4,7 @@ import guru.springframework.domain.*;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.RecipeRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
-import guru.springframework.repositories.reactive.CategoryReactiveRepository;
-import guru.springframework.repositories.reactive.RecipeReactiveRepository;
-import guru.springframework.repositories.reactive.UnitOfMeasureReactiveRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -30,13 +26,6 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     private final RecipeRepository recipeRepository;
     private final UnitOfMeasureRepository unitOfMeasureRepository;
 
-    @Autowired
-    RecipeReactiveRepository recipeReactiveRepository;
-    @Autowired
-    CategoryReactiveRepository categoryReactiveRepository;
-    @Autowired
-    UnitOfMeasureReactiveRepository unitOfMeasureReactiveRepository;
-
     public RecipeBootstrap(CategoryRepository categoryRepository,
                            RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
         this.categoryRepository = categoryRepository;
@@ -51,10 +40,6 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         loadUom();
         recipeRepository.saveAll(getRecipes());
         log.debug("Loading Bootstrap Data");
-
-        log.debug("Reactive Recipe Count: " + recipeReactiveRepository.count().block());
-        log.debug("Reactive Category Count: " + categoryReactiveRepository.count().block());
-        log.debug("Reactive UnitOfMeasure Count: " + unitOfMeasureReactiveRepository.count().block());
     }
 
     private void loadCategories(){
